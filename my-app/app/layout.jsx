a532a76382f054a0ddb730e816bbc4b494d5323c
Marko <kostres90@hotmail.com>
2024-08-25 18:48:45 +0200

    chore: Add Next.js configuration files and update package dependencies

---
+++ my-app/app/layout.jsx
@@ -0,0 +1,12 @@
+import "@/assets/styles/globals.css";
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
