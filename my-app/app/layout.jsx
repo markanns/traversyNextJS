0a4981a6bf671d95e5a130a55c74b16613643f49
Marko <kostres90@hotmail.com>
2024-08-31 21:00:46 +0200

    chore: Add Next.js configuration files and update package dependencies

--- my-app/app/layout.jsx
+++ my-app/app/layout.jsx
@@ -1,4 +1,9 @@
 import "@/assets/styles/globals.css";
+export const metadata = {
+  title: "My App",
+  description: "My awesome app",
+  keywords: "realestate, houses, homes",
+};
 const MainLayout = ({ children }) => {
   return (
     <html>
